5bcca64ba8085b0543d2fac6cf323cd399d20a34
Thomas Maier <thomas04.maier@lindner-group.com>
2025-03-06 09:44:13 +0100

    Make the main better

--- example/main.go
+++ example/main.go
@@ -11,9 +11,15 @@ func main() {
 	sz := statez.Statez{}
 
 	testSvc := statez.NewServiceHandlerWithOpts("mqtt handler")
-	testSvc.StatusIgnore()
+	testSvc.StateReady()
 
-	sz.RegisterService(testSvc)
+	test2Svc := statez.NewServiceHandlerWithOpts("web server")
+	test2Svc.StateReady()
+
+	test3Svc := statez.NewServiceHandlerWithOpts("database")
+	test3Svc.StateIgnore()
+
+	sz.RegisterService(testSvc, test2Svc, test3Svc)
 
 	http.HandleFunc("/ready", sz.ReadynessHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
